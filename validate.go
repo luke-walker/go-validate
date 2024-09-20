b147193d8c979c35ce8677b2cc58cbe146b4eb5e
Luke Walker <lukeryanwalker2000@gmail.com>
2024-09-20 15:41:17 -0700

    v0.2.0 - Add URL-encoded form support
    - URL-encoded form support
    
    - Rename ValidateJSON -> ValidateData

--- validate.go
+++ validate.go
@@ -17,28 +17,45 @@ type Validator struct {
     Fields FieldsMap
 }
 
-func (v Validator) ValidateJSON(next http.Handler) http.Handler {
+func (v Validator) ValidateData(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        var bodyBuf bytes.Buffer
-        body := io.TeeReader(r.Body, &bodyBuf)
-        r.Body = io.NopCloser(&bodyBuf)
-
         var data map[string]any
-        if err := json.NewDecoder(body).Decode(&data); err != nil {
-            http.Error(w, "Invalid request payload (JSON expected)", http.StatusBadRequest)
+        switch contentType := r.Header.Get("Content-Type"); contentType {
+        case "application/x-www-form-urlencoded":
+            if err := r.ParseForm(); err != nil {
+                http.Error(w, "Invalid request payload (URL-encoded form expected)", http.StatusBadRequest)
+                return
+            }
+            data = make(map[string]any)
+            for key, values := range r.PostForm {
+                if len(values) == 1 {
+                    data[key] = values[0]
+                } else {
+                    data[key] = values
+                }
+            }
+            break
+        case "application/json":
+            var bodyBuf bytes.Buffer
+            body := io.TeeReader(r.Body, &bodyBuf)
+            r.Body = io.NopCloser(&bodyBuf)
+            if err := json.NewDecoder(body).Decode(&data); err != nil {
+                http.Error(w, "Invalid request payload (JSON expected)", http.StatusBadRequest)
+                return
+            }
+            break
+        default:
+            http.Error(w, fmt.Sprintf("Unsupported request payload (%s)", contentType), http.StatusBadRequest)
             return
         }
 
         for field, cons := range v.Fields {
-            _, ok := data[field]
-            if !ok {
-                required, ok := cons["required"].(bool)
-                if !ok || !required {
-                    continue
+            if _, ok := data[field]; !ok {
+                if required, ok := cons["required"].(bool); ok && required {
+                    http.Error(w, fmt.Sprintf("Missing required field '%s'", field), http.StatusBadRequest)
+                    return
                 }
-
-                http.Error(w, fmt.Sprintf("Missing required field '%s'", field), http.StatusBadRequest)
-                return
+                continue
             }
         }
 
